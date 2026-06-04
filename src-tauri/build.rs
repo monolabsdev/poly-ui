52562e6d0e82379fcc027b898ac8cec489ea7e59
Theo Slater <155472213+TheoSlater@users.noreply.github.com>
2026-06-04 14:47:52 +0100

    Update build.rs

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,9 +1,3 @@
 fn main() {
     tauri_build::build();
-
-    if cfg!(target_os = "windows") {
-        if std::env::var_os("CARGO_FEATURE_DICTATION").is_some() {
-            println!("cargo:rustc-link-arg=/NODEFAULTLIB:libvcruntime.lib");
-        }
-    }
 }
